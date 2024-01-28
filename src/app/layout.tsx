0293ded460d68163c9561f8676a61dccfcd2d62e
Naoto Hori <naoto.hori@gmail.com>
2024-01-28 11:24:10 +0900

    biome setup and tailwindcss completed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
-import '@mantine/core/styles.css';
 import React from 'react';
 import { MantineProvider, ColorSchemeScript } from '@mantine/core';
 import { theme } from './theme';
+
+import './globals.css';
 
 export const metadata = {
   title: 'Mantine Next.js template',
